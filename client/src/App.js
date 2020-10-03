9b1467feb44aabf422bd298bf26844bdad3e8835
muddybootscode <firsttogo98@gmail.com>
2020-10-03 08:37:53 -0400

    refactor: Updated docker-compose.yml to add hot reload

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React from 'react';
 
 function App() {
-  return <div className='App'>Apollo React Template</div>;
+  return <div className='App'>Apollo React Template - Made Changes and then</div>;
 }
 
 export default App;
