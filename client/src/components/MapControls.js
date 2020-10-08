1a3063454ae50a0855311e868b815ed9f5310e24
muddybootscode <firsttogo98@gmail.com>
2020-10-08 10:25:56 -0400

    Feature: Adding map controls

--- client/src/components/MapControls.js
+++ client/src/components/MapControls.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import Map from './Map';
-import ContainerDimensions from 'react-container-dimensions';
+import { makeStyles } from '@material-ui/core/styles';
+import { Divider, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'grid',
+    gridTemplateColumns: '1fr 3fr',
+  },
+  controls: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 10,
+  },
+  hr: {
+    width: '80%',
+    color: 'black',
+  },
+}));
 
 const MapControls = () => {
+  const classes = useStyles();
   return (
-    <div id='map-controls' style={{ position: 'relative' }}>
-      <Map />
+    <div className={classes.root}>
+      <div className={classes.controls}>
+        <Typography variant='h6'>Map Controls</Typography>
+        <hr className={classes.hr} />
+      </div>
+      <div id='map-controls' style={{ position: 'relative' }}>
+        <Map />
+      </div>
     </div>
   );
 };
